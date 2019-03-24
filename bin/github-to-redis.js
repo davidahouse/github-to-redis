9d8cb5f497be9508bde85eaa2867f53893588c50
David House <david.house@kp.org>
2019-03-24 18:56:51 -0400

    :tada: first

---
+++ bin/github-to-redis.js
@@ -0,0 +1,58 @@
+#!/usr/bin/env node
+const chalk = require('chalk')
+const clear = require('clear')
+const figlet = require('figlet')
+const redis = require('redis')
+let express = require('express')
+let app = express()
+let router = express.Router() // eslint-disable-line new-cap
+let bodyParser = require('body-parser')
+app.use(bodyParser.json())
+app.use('/', router)
+
+const conf = require('rc')('ghtoredis', {
+  // defaults
+  redisHost: 'localhost',
+  redisPort: 6379,
+  redisPassword: null,
+  queueName: 'github',
+  webPort: 7766
+})
+
+let client = createRedisClient()
+
+client.on('error', function(err) {
+  console.log('redis connect error: ' + err)
+})
+
+router.post('/github', function(req, res) {
+
+  const ghEvent = {}
+  ghEvent.id = req.headers['x-github-delivery']
+  ghEvent.event = req.headers['x-github-event']
+  ghEvent.payload = req.body
+
+  client.rpush(conf.queueName, JSON.stringify(ghEvent))
+
+  console.log(chalk.green('--> ' + ghEvent.id + ' ' + ghEvent.event))
+  let ok = {'response': 'ok'}
+  res.json(ok)
+})
+
+clear()
+console.log(chalk.red(figlet.textSync('github to redis', {horizontalLayout: 'full'})))
+
+app.listen(conf.webPort, function() {
+  console.log(chalk.yellow('Listening on port: ' + conf.webPort))
+})
+
+function createRedisClient() {
+  if (conf.redisPassword != null) {
+    return redis.createClient({host: conf.redisHost, 
+                               port: conf.redisPort, 
+                               password: conf.redisPassword})
+  } else {
+    return redis.createClient({host: conf.redisHost, 
+                               port: conf.redisPort})
+  }
+}
